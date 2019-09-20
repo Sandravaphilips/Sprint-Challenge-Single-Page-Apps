895f581e0a5c9022e96917060d69335d4327b788
Sandrava Philips <lacasandra1@gmail.com>
2019-09-20 10:07:02 +0100

    styled the home page

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import Header from "./components/Header.js";
 import CharacterList from "./components/CharacterList";
 import HiddenCharacters from "./HiddenCharacters";
 import { Route } from "react-router-dom";
+import styled from "styled-components";
+
+const StyleHomePage = styled.main`
+  background-color: #202329;
+  color: white;
+`;
 
 export default function App() {
   const hiddenCharacters = [];
   return (
-    <main>
+    <StyleHomePage>
       <Header />
       <HiddenCharacters characters={hiddenCharacters} />
       <Route path="/characters" component={CharacterList} />
-    </main>
+    </StyleHomePage>
   );
 }
--- src/HiddenCharacters.js
+++ src/HiddenCharacters.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import CharacterList from './components/CharacterList';
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const StyleLink = styled(Link)`
+    text-decoration: none;
+    
+    &:hover {
+        text-decoration: underline;
+    }
+`;
 
 
-export default  function HiddenCharacters({characters}) {
+
+export default function HiddenCharacters({characters}) {
     return (
-        <div className="saved-list">
+        <div>
             <h3>Characters:</h3>
             {characters.map(character => (
             <span>{character.name}</span>
             ))}
-            <Link to='/characters' component={CharacterList}>
+            <StyleLink to='/characters' component={CharacterList}>
                 <div>View Characters</div>
-            </Link>
+            </StyleLink>
         </div>
     )
 };
